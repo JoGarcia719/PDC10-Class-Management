4c8a3e5761c33556aee7adbcfc9582ce6286cdf3
JoGarcia719 <garcia.josephandrew@auf.edu.ph>
2022-10-15 16:01:04 +0800

    added some changes

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>School Menu System</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body bgcolor="green">
+<style>
+
+</style>
+    </head>
+
+
+<!-- Sidebar -->
+<div class="w3-sidebar w3-light-green w3-bar-block" style="width:25%">
+  <h3 class="w3-bar-item">Dashboard</h3>
+  <a href="#" class="w3-bar-item w3-button w3-yellow">Students</a>
+  <a href="#" class="w3-bar-item w3-button w3-orange">Teachers</a>
+  <a href="#" class="w3-bar-item w3-button w3-yellow">Class Rosters</a>
+</div>
+
+<!-- Page Content -->
+<div style="margin-left:25%">
+
+<div class="w3-container w3-orange">
+  <h1>School Management System</h1>
+</div>
+
+
+<div class="w3-container w3-yellow">
+<h2>Angeles University Foundation School System</h2>
+<p>Bigger Better Smarter Stronger bgcolor=orange</p>
+</div>
+
+</div>
+      
+</body>
+</html>
---
+++ models/TrainingClass.php
@@ -0,0 +1,20 @@
+<?php
+
+class TrainingClass
+{
+    protected $id;
+    protected $name;
+    protected $descrption;
+    protected $code;
+    protected $teacher_id;
+
+    public function createClass($name, $description, $code)
+    {
+        // name, description, code
+    }
+    public function assignTeacher($teacherID, $teacherName)
+       // teacherID, teacherName 
+    {
+    public function
+    }
+    public function 
